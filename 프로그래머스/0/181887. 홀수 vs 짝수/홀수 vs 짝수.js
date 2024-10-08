2f41569e55b4ca9e57ac43d6a1fbcf08a751fd3a
임한성 <164149491+imhanseong@users.noreply.github.com>
2024-10-08 14:51:12 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,20 @@
+function solution(num_list) {
+    var answer = 0;
+    let a = 0;
+    let b = 0;
+    for(let i=0; i < num_list.length; i++){
+        if(i % 2 != 0){
+            a += num_list[i]
+        }
+        else{
+            b += num_list[i]
+        }
+    }
+    if(a > b){
+        answer += a;
+    }
+    else{
+        answer += b
+    }
+    return answer;
+}
